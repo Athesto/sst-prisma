ee8b460b17402e16ad6012d9d38a15b025d39ba1
Athesto <gamez.live@gmail.com>
2023-03-06 10:35:34 -0500

    include api endpoint

--- stacks/restApi.ts
+++ stacks/restApi.ts
@@ -3,7 +3,9 @@ import { Api, StackContext } from 'sst/constructs';
 export function RestApi({ stack }: StackContext) {
   const api = new Api(stack, 'RestApi', {
     defaults: { function: { timeout: '10 seconds' } },
-    routes: {}, // can add HTTP routes here - https://docs.serverless-stack.com/constructs/Api#using-the-minimal-config
+    routes: {
+      "GET /demo": "./backend/src/api/resolver/greeting.getGreeting",
+    }, // can add HTTP routes here - https://docs.serverless-stack.com/constructs/Api#using-the-minimal-config
   });
   stack.addOutputs({
     RestApiEndpoint: api.url,
